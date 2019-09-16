a445c1f6fac496b70d1c9e1028a1c36a6cd0f07e
iourisorokine <isorokine@gmail.com>
2019-09-16 12:00:58 +0200

    create basic routing structure v1.0

--- models/User.js
+++ models/User.js
@@ -1,49 +1,46 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema(
-  {
-    name: {
-      type: String,
-      required: true
-    },
-    password: {
-      type: String,
-      required: true
-    },
-    email: {
-      type: String,
-      required: true
-    },
-    city: {
-      type: String,
-      required: true
-    },
-    level: {
-      type: String,
-      enum: ["novice", "beginner", "intermediate", "advanced", "senior"],
-      required: true,
-    },
-    mentorship: {
-      type: String,
-      enum: ["Yes", "No"],
-
-    },
-    connections: Array,
-    interests: {
-      type: Array,
-      required: true,
-    }
-    pinnedContent: Array,
-    githubLink: String
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true
   },
-  {
-    timestamps: {
-      createdAt: "created_at",
-      updatedAt: "updated_at"
-    }
+  password: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  city: {
+    type: String,
+    required: true
+  },
+  level: {
+    type: String,
+    enum: ["novice", "beginner", "intermediate", "advanced", "senior"],
+    required: true,
+  },
+  mentorship: {
+    type: String,
+    enum: ["Yes", "No"],
+
+  },
+  connections: Array,
+  interests: {
+    type: Array,
+    required: true,
+  },
+  pinnedContent: Array,
+  githubLink: String
+}, {
+  timestamps: {
+    createdAt: "created_at",
+    updatedAt: "updated_at"
   }
-);
+});
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+// router.get('/user/feed', (req, res, next) => {
+//   res.render('user/feed');
+// });
+
+// router.get('/user/pinned', (req, res, next) => {
+//   res.render('user/pinned-content');
+// });
+
+// router.get('/user/profile', (req, res, next) => {
+//   res.render('user/profile');
+// });
 
 module.exports = router;
