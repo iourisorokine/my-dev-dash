756260180750f19511306f5f1befb3598dac6668
Andre <itstheandre@gmail.com>
2019-09-16 18:03:51 +0200

    stuck with image upload. Will wait for TA help to figure out what is wrong

--- routes/user.js
+++ routes/user.js
@@ -3,27 +3,26 @@ const router = express.Router();
 const axios = require("axios");
 
 // manages the nees feed from the news API
-router.get('/feed', (req, res, next) => {
-
+router.get("/feed", (req, res, next) => {
   //declare variables that manage the api calls according to interests
   let newsList;
-  let todaysDate = new Date()
+  let todaysDate = new Date();
   const interests = req.user.interests;
   let interestsStr = interests.reduce((acc, val, index) => {
-    if (index < interests.length - 1) return acc + val + '+';
+    if (index < interests.length - 1) return acc + val + "+";
     return acc + val;
-  }, "")
+  }, "");
   const requestUrl = `https://newsapi.org/v2/everything?q=${interestsStr}&language=en&from=${todaysDate}&sortBy=popularity&apiKey=${process.env.NEWS_API_KEY}`;
 
-  axios.get(requestUrl)
-    .then(response => {
-      const newsList = response.data.articles;
-      // topicsList = topicsList.concat(newsList)
-      // newsList = [...news,...newsList]
-      res.render('user/feed', {
-        newsList
-      });
-    })
+  axios.get(requestUrl).then(response => {
+    const newsList = response.data.articles;
+    // topicsList = topicsList.concat(newsList)
+    // newsList = [...news,...newsList]
+    res.render("user/feed", {
+      newsList,
+      user: req.user
+    });
+  });
 });
 
 router.get("/pinned", (req, res, next) => {
