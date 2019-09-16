f06c4c6460156f7e1e6a6f4582a1f1cebbd1f737
Andre <itstheandre@gmail.com>
2019-09-16 10:52:19 +0200

    User model created

--- models/User.js
+++ models/User.js
@@ -1,15 +1,32 @@
-const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  username: String,
-  password: String
-}, {
-  timestamps: {
-    createdAt: 'created_at',
-    updatedAt: 'updated_at'
+const userSchema = new Schema(
+  {
+    name: String,
+    password: String,
+    email: String,
+    city: String,
+    level: {
+      type: String,
+      enum: ["novice", "beginner", "intermediate", "advanced", "senior"]
+    },
+    mentorship: {
+      type: String,
+      enum: ["Yes", "No"]
+    },
+    connections: Array,
+    interests: Array,
+    pinnedContent: Array,
+    githubLink: String
+  },
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at"
+    }
   }
-});
+);
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 module.exports = User;
