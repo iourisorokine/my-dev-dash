971fc3c9a30cd9091b8f78affaae79349998af57
Andre <itstheandre@gmail.com>
2021-01-03 14:42:48 +0100

    fix(mongo) add new mongo driver

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ mongoose
   })
   .then((x) => {
     console.log(
-      `Connected to Mongo! Database name: "${x.connections[0].name}"`,
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
     );
   })
   .catch((err) => {
@@ -29,7 +29,7 @@ mongoose
 
 const app_name = require("./package.json").name;
 const debug = require("debug")(
-  `${app_name}:${path.basename(__filename).split(".")[0]}`,
+  `${app_name}:${path.basename(__filename).split(".")[0]}`
 );
 
 const app = express();
@@ -40,7 +40,7 @@ app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
     extended: false,
-  }),
+  })
 );
 app.use(cookieParser());
 
@@ -51,7 +51,7 @@ app.use(
     src: path.join(__dirname, "public"),
     dest: path.join(__dirname, "public"),
     sourceMap: true,
-  }),
+  })
 );
 
 app.set("views", path.join(__dirname, "views"));
@@ -81,7 +81,7 @@ app.use(
     store: new MongoStore({
       mongooseConnection: mongoose.connection,
     }),
-  }),
+  })
 );
 app.use(flash());
 require("./passport")(app);
